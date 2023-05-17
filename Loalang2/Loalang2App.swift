665f6b6141344ad7e579f41f1b3b4307138191ea
Sangyoon <qlrqod123123@naver.com>
2023-05-17 23:48:41 +0900

    Initial Commit

---
+++ Loalang2/Loalang2App.swift
@@ -0,0 +1,17 @@
+//
+//  Loalang2App.swift
+//  Loalang2
+//
+//  Created by 채상윤 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct Loalang2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
